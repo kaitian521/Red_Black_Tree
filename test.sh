aa9663042887ccb2d3dec69651e6ffc99dd18278
lishaokai <kaitian521@gmail.com>
2012-03-07 15:31:33 +0800

    second commit,add  red-black tree

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
   git init
   git add *
   git commit -m 'second commit,add  red-black tree'
+  git remote rm origin
   git remote add origin git@github.com:kaitian521/Red_Black_Tree.git
   git push -u origin master
 
